8d3622110b210d28cee88325186b6c1c463be6e1
martinamattioli <63170690+martinamattioli@users.noreply.github.com>
2020-05-31 18:07:33 -0300

    Sin errores

--- TP Cuatrimestral.hs
+++ TP Cuatrimestral.hs
@@ -1,5 +1,3 @@
-import.Text.Show.Functions ()
-
 type Patente = String
 type Desgaste = Float
 type Fecha = (Int, Int, Int)
@@ -15,45 +13,59 @@ data Auto = UnAuto { patente :: Patente,
                      ultimoArreglo :: Fecha
                    } deriving (Show)
 
--- Auxiliares
+{-
+ El auto tiene: 
+-la patente, que puede tener formato viejo “RVM363” o el nuevo “AB808RD”
+-el desgaste de cada una de las llantas, ej: [ 0.5, 0.1, 0, 0.2 ]
+-las revoluciones por minuto a las que regula el motor, ej: 1500
+-la temperatura del agua luego de 5 minutos de encendido el auto: 90
+-la fecha del último arreglo
+-}
 
 anio :: Fecha -> Int
 anio (_,_,year) = year
 
-segundoDeLaLista :: String -> Char
-segundoDeLaLista lista = lista !! 1
+-- Auxiliares
+
+mapDesgaste :: ([Desgaste] -> [Desgaste]) -> Auto -> Auto
+mapDesgaste unaFuncion unAuto = unAuto {desgasteDeLlantas = unaFuncion.desgasteDeLlantas $ unAuto}
+
+mapUltimoArreglo :: (Fecha -> Fecha) -> Auto -> Auto
+mapUltimoArreglo unaFuncion unAuto = unAuto {ultimoArreglo = unaFuncion.ultimoArreglo $ unAuto}
 
 -- Auxiliares
 
--- 1)
 
-costoDeReparacion :: Auto -> Int
-costoDeReparacion unAuto 
- | patenteTieneSieteDigitos (patente unAuto) = 12500 
- | patenteEstaEntreDJyNB (patente unAuto) && patenteTerminaEn4 (patente unAuto)  = 3 * (length (patente unAuto))
- | patenteEstaEntreDJyNB (patente unAuto)  = 20000
+-- 1)
+-- Hay que usar composicion
+
+costoDeReparacion :: Patente -> Auto -> Int
+costoDeReparacion unaPatente unAuto 
+ | patenteTieneSieteDigitos unaPatente = 12500 
+ | patenteEstaEntreDJyNB unaPatente && patenteTerminaEn4 unaPatente  = 3 * (length unaPatente)
+ | patenteEstaEntreDJyNB unaPatente  = 20000
+ -- logicamente habria que pasarles un auto, porque es la patente de ESE auto pero hay q ver como hacer las funciones para que RECIBAn un auto y no tiren ERROOOOORRRRR
  | otherwise = 15000
 
 patenteTieneSieteDigitos :: Patente -> Bool
 patenteTieneSieteDigitos unaPatente = (== 7).length $ unaPatente 
-
-patenteTerminaEn4 :: Patente -> Bool
-patenteTerminaEn4 unaPatente = (==4). drop ((length unaPatente) - 1)  $ unaPatente 
+-- Voy a tener que aprender a usar ese signo de inte
+{-
+patenteTieneSieteDigitos :: Patente -> Bool
+patenteTieneSieteDigitos unaPatente = (== 7).length unaPatente 
+-}
 
 patenteEstaEntreDJyNB :: Patente -> Bool
-patenteEstaEntreDJyNB unaPatente = estaEntreDyN unaPatente && estaEntreJyB unaPatente
+patenteEstaEntreDJyNB unaPatente = estaEntreDyN (head unaPatente) && estaEntreJyB (tail (take 2 unaPatente))
 
-entreDyN :: Char -> Bool
-entreDyN unaLetra = elem unaLetra ['D'..'N']
+estaEntreDyN :: Char -> Bool
+estaEntreDyN unaLetra = elem unaLetra ['D'..'N']
 
-entreJyB :: Char -> Bool
-entreJyB unaLetra = elem unaLetra ['J'..'B']
+estaEntreJyB :: Char -> Bool
+estaEntreJyB unaLetra = elem unaLetra ['J'..'B']
 
-estaEntreDyN :: Patente -> Bool
-estaEntreDyN = entreDyN.head 
-
-estaEntreJyB :: Patente -> Bool
-estaEntreJyB = entreJyB.segundoDeLaLista.take 2
+patenteTerminaEn4 :: Patente -> Bool
+patenteTerminaEn4 unaPatente =  drop ((length unaPatente) - 1) unaPatente == 4
 
 -- 2)
 
@@ -74,7 +86,6 @@ anioDelUltimoArreglo :: Auto -> Int
 anioDelUltimoArreglo  = anio.ultimoArreglo 
 
 -- 3) Taller Mecanico
-
 -- Integrante a
 
 alfa :: TallerMecanico
@@ -89,7 +100,7 @@ regularLasVueltas :: Int -> TallerMecanico
 regularLasVueltas revoluciones unAuto = unAuto {rpm = revoluciones} 
 
 bravo :: Tecnico
-bravo unAuto = unAuto {desgasteDeLlantas = [0.0,0.0,0.0,0.0]}
+bravo unAuto = unAuto {desgasteDeLlantas = [0]}  -- o seria = 0 nada mas?
 
 charly :: Tecnico 
 charly = alfa.bravo 
@@ -117,22 +128,21 @@ ordenamientoTOC :: [Auto] -> Bool
 ordenamientoTOC listaDeAutos = all odd (map desgasteDelAuto (elementosImpares listaDeAutos)) && all even (map desgasteDelAuto (elementosPares listaDeAutos))
 
 elementosImpares :: [a] -> [a]
-elementosImpares [] = []
 elementosImpares [elemento] = filter odd [elemento]
 elementosImpares [primero,segundo] = filter odd [primero,segundo]
-elementosImpares (primero:_:cola) = (primero:elementosImpares(cola))
+elementosImpares (primero:segundo:cola) = (primero:numerosImpares(cola))
 
 elementosPares :: [a] -> [a]
-elementosPares [] = []
 elementosPares [elemento] = filter even [elemento]
 elementosPares [primero,segundo] = filter even [primero,segundo]
-elementosPares (_:segundo:cola) = (segundo:elementosPares(cola))
+elementosPares (primero:segundo:cola) = (segundo:numerosImpares(cola))
 
 desgasteDelAuto :: Auto -> Int
-desgasteDelAuto unAuto = round.(10 *).sum.desgasteDeLlantas $ unAuto
+desgasteDelAuto unAuto = round (10 * (sum (desgasteDeLlantas unAuto)))
+
 
 -- 5)
-
+ 
 ordenDeReparacion :: Fecha -> [Tecnico] -> TallerMecanico
 ordenDeReparacion unaFecha unosTecnicos unAuto = (actualizarUltimaFechaDeReparacion unaFecha.realizarLasReparaciones unosTecnicos) unAuto
 
@@ -145,31 +155,24 @@ realizarLasReparaciones unosTecnicos unAuto = foldl1 (.) unosTecnicos $ unAuto
 -- 6)
 -- Integrante a
 
-{-
 tecnicosQueDejanElAutoEnCondiciones ::  Auto -> [Tecnico] -> [Tecnico]
-tecnicosQueDejanElAutoEnCondiciones unAuto unosTecnicos = filter (dejaElAutoEnCondiciones unosTecnicos unAuto) unosTecnicos
+tecnicosQueDejanElAutoEnCondiciones unAuto unosTecnicos = filter (dejaElAutoEnCondiciones unosTecnicos) unAuto 
 
-tecnicosQueDejanElAutoEnCondiciones' ::  Auto -> [Tecnico] -> [Auto]
-tecnicosQueDejanElAutoEnCondiciones' unAuto unosTecnicos = filter autoEnCondiciones (arreglarAutos unosTecnicos unAuto) 
-                 -- Esto funciona v ^
-arreglarAutos :: [Tecnico] -> Auto -> [Auto]
-arreglarAutos unosTecnicos unAuto = map ($unAuto) unosTecnicos
+dejaElAutoEnCondiciones :: [Tecnico] -> Auto -> Bool
+dejaElAutoEnCondiciones unosTecnicos unAuto = autoEnCondiciones.realizarLasReparaciones unosTecnicos $ unAuto
+
+
+--tecnicosQueDejanElAutoEnCondiciones unosTecnicos unAuto = filter autoEnCondiciones (map ($unAuto) unosTecnicos)
+-- ahora necesito pasarle a tecnicosQueDejanElAutoEnCondiciones esa funcion y una lista de tecnicos, para que me devuelva
 
 
 autoEnCondiciones :: Auto -> Bool
 autoEnCondiciones = not.esUnAutoPeligroso 
--}
-
-tecnicosQueDejanElAutoEnCondiciones :: [Tecnico] -> Auto ->  [Tecnico] 
-tecnicosQueDejanElAutoEnCondiciones unosTecnicos unAuto = filter (arreglarAuto unAuto) unosTecnicos
-
-arreglarAuto :: Auto -> Tecnico -> Bool
-arreglarAuto unAuto unTecnico = (esUnAutoPeligroso.unTecnico)  unAuto 
 
 -- Integrante b
 
-costoDeReparacionDeAutosQueNecesitanRevision :: [Auto] -> [Int]
-costoDeReparacionDeAutosQueNecesitanRevision unosAutos = map costoDeReparacion.autosQueNecesitanRevision $ unosAutos
+costoDeReparacionDeAutosQueNecesitanRevision :: [Patente] -> [Auto] -> Int
+costoDeReparacionDeAutosQueNecesitanRevision unasPatentes unosAutos = costoDeReparacion unasPatentes.autosQueNecesitanRevision unosAutos  
 
 autosQueNecesitanRevision :: [Auto] -> [Auto]
 autosQueNecesitanRevision unosAutos = filter necesitaRevision unosAutos
@@ -181,7 +184,6 @@ autosQueNecesitanRevision unosAutos = filter necesitaRevision unosAutos
 -- Si se puede hacer con una lista infinita ya que la funcion head trabaja a partir de una "evaluacion perezosa"
 -- o lazy evaluation, entonces va a evaluar solo lo que necesita y despues tomar esos parametros.
 -- Ej.
-primerTecnicoEnCondiciones :: [Tecnico] -> Auto -> Tecnico
 primerTecnicoEnCondiciones unosTecnicos unAuto = head (tecnicosQueDejanElAutoEnCondiciones unosTecnicos unAuto)
 
 --Integrante b
