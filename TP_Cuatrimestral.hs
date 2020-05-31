b1cfbaa488862c54f44c8353115da8f6f2ed54c2
Matiduraan <63005534+Matiduraan@users.noreply.github.com>
2020-05-31 20:25:11 -0300

    Update TP_Cuatrimestral.hs

--- TP_Cuatrimestral.hs
+++ TP_Cuatrimestral.hs
@@ -1,12 +1,11 @@
-import.Text.Show.Functions ()
+import Text.Show.Functions ()
 
 type Patente = String
 type Desgaste = Float
 type Fecha = (Int, Int, Int)
+
 type TallerMecanico = Auto -> Auto
 type Tecnico = TallerMecanico
-
-
 
 data Auto = UnAuto { patente :: Patente,
                      desgasteDeLlantas :: [Desgaste],
@@ -14,6 +13,35 @@ data Auto = UnAuto { patente :: Patente,
                      temperaturaAgua :: Int,
                      ultimoArreglo :: Fecha
                    } deriving (Show)
+
+
+-- Ejemplos de autos
+autoMartu1 :: Auto
+autoMartu1 = UnAuto "AT001LN" [0.1,0.4, 0.2, 0.0] 100 120 (10,04,2020)
+
+autoMartu2 :: Auto
+autoMartu2 = UnAuto "DJV214" [0.8,0.2,0.4,0.5] 2001 200 (23,05,2018)
+
+autoMartu3 :: Auto
+autoMartu3 = UnAuto "DJV215" [0.5, 0.1, 0.6, 0.4] 1000 90 (12,07,2016)
+
+autoMati1 :: Auto
+autoMati1 = UnAuto "AAA111" [0.51, 0.1, 0.6, 0.4] 12 50 (1,2,2015)
+
+autoMati2 :: Auto
+autoMati2 = UnAuto "DFH029" [0.5, 0.1, 0.6, 0.4] 50 0 (1,12,2015)
+
+autoMati3 :: Auto
+autoMati3 = UnAuto "NBA158" [0.1, 0.1, 0.1, 0] 0 12 (5,10,2016)
+
+autoMati5 :: Auto
+autoMati5 = UnAuto "NBA158" [0.2, 0.5, 0.6, 0.1] 0 12 (5,10,2016)
+
+autoMati4 :: Auto
+autoMati4 = UnAuto "NBA158" [0.1, 0.4, 0.2, 0] 0 12 (5,10,2016)
+
+listaDeAutosDePrueba :: [Auto]
+listaDeAutosDePrueba = [autoMartu1, autoMartu2, autoMartu3, autoMati1, autoMati2, autoMati3, autoMati4]
 
 -- Auxiliares
 
@@ -30,7 +58,7 @@ segundoDeLaLista lista = lista !! 1
 costoDeReparacion :: Auto -> Int
 costoDeReparacion unAuto 
  | patenteTieneSieteDigitos (patente unAuto) = 12500 
- | patenteEstaEntreDJyNB (patente unAuto) && patenteTerminaEn4 (patente unAuto)  = 3 * (length (patente unAuto))
+ | patenteEstaEntreDJyNB (patente unAuto) && patenteTerminaEn4 (patente unAuto)  = 3000 * (length (patente unAuto))
  | patenteEstaEntreDJyNB (patente unAuto)  = 20000
  | otherwise = 15000
 
@@ -38,7 +66,10 @@ patenteTieneSieteDigitos :: Patente -> Bool
 patenteTieneSieteDigitos unaPatente = (== 7).length $ unaPatente 
 
 patenteTerminaEn4 :: Patente -> Bool
-patenteTerminaEn4 unaPatente = (==4). drop ((length unaPatente) - 1)  $ unaPatente 
+patenteTerminaEn4 unaPatente = esPatenteVieja unaPatente && last unaPatente == '4' 
+
+esPatenteVieja :: Patente -> Bool
+esPatenteVieja = not.patenteTieneSieteDigitos
 
 patenteEstaEntreDJyNB :: Patente -> Bool
 patenteEstaEntreDJyNB unaPatente = estaEntreDyN unaPatente && estaEntreJyB unaPatente
@@ -47,13 +78,13 @@ entreDyN :: Char -> Bool
 entreDyN unaLetra = elem unaLetra ['D'..'N']
 
 entreJyB :: Char -> Bool
-entreJyB unaLetra = elem unaLetra ['J'..'B']
+entreJyB unaLetra = elem unaLetra (['A','B'] ++ ['J'..'Z']) 
 
 estaEntreDyN :: Patente -> Bool
 estaEntreDyN = entreDyN.head 
 
 estaEntreJyB :: Patente -> Bool
-estaEntreJyB = entreJyB.segundoDeLaLista.take 2
+estaEntreJyB = entreJyB.segundoDeLaLista
 
 -- 2)
 
@@ -89,7 +120,7 @@ regularLasVueltas :: Int -> TallerMecanico
 regularLasVueltas revoluciones unAuto = unAuto {rpm = revoluciones} 
 
 bravo :: Tecnico
-bravo unAuto = unAuto {desgasteDeLlantas = [0.0,0.0,0.0,0.0]}
+bravo unAuto = unAuto {desgasteDeLlantas = [0.0,0.0,0.0,0.0]} 
 
 charly :: Tecnico 
 charly = alfa.bravo 
@@ -118,14 +149,14 @@ ordenamientoTOC listaDeAutos = all odd (map desgasteDelAuto (elementosImpares li
 
 elementosImpares :: [a] -> [a]
 elementosImpares [] = []
-elementosImpares [elemento] = filter odd [elemento]
-elementosImpares [primero,segundo] = filter odd [primero,segundo]
+elementosImpares [elemento] = [elemento]
+elementosImpares [_,segundo] = [segundo]
 elementosImpares (primero:_:cola) = (primero:elementosImpares(cola))
 
 elementosPares :: [a] -> [a]
 elementosPares [] = []
-elementosPares [elemento] = filter even [elemento]
-elementosPares [primero,segundo] = filter even [primero,segundo]
+elementosPares [_] = []
+elementosPares [_,segundo] = [segundo]
 elementosPares (_:segundo:cola) = (segundo:elementosPares(cola))
 
 desgasteDelAuto :: Auto -> Int
@@ -143,28 +174,14 @@ realizarLasReparaciones :: [Tecnico] -> TallerMecanico
 realizarLasReparaciones unosTecnicos unAuto = foldl1 (.) unosTecnicos $ unAuto
 
 -- 6)
+
 -- Integrante a
-
-{-
-tecnicosQueDejanElAutoEnCondiciones ::  Auto -> [Tecnico] -> [Tecnico]
-tecnicosQueDejanElAutoEnCondiciones unAuto unosTecnicos = filter (dejaElAutoEnCondiciones unosTecnicos unAuto) unosTecnicos
-
-tecnicosQueDejanElAutoEnCondiciones' ::  Auto -> [Tecnico] -> [Auto]
-tecnicosQueDejanElAutoEnCondiciones' unAuto unosTecnicos = filter autoEnCondiciones (arreglarAutos unosTecnicos unAuto) 
-                 -- Esto funciona v ^
-arreglarAutos :: [Tecnico] -> Auto -> [Auto]
-arreglarAutos unosTecnicos unAuto = map ($unAuto) unosTecnicos
-
-
-autoEnCondiciones :: Auto -> Bool
-autoEnCondiciones = not.esUnAutoPeligroso 
--}
 
 tecnicosQueDejanElAutoEnCondiciones :: [Tecnico] -> Auto ->  [Tecnico] 
 tecnicosQueDejanElAutoEnCondiciones unosTecnicos unAuto = filter (arreglarAuto unAuto) unosTecnicos
 
 arreglarAuto :: Auto -> Tecnico -> Bool
-arreglarAuto unAuto unTecnico = (esUnAutoPeligroso.unTecnico)  unAuto 
+arreglarAuto unAuto unTecnico = not.esUnAutoPeligroso.unTecnico $ unAuto 
 
 -- Integrante b
 
@@ -180,7 +197,9 @@ autosQueNecesitanRevision unosAutos = filter necesitaRevision unosAutos
 
 -- Si se puede hacer con una lista infinita ya que la funcion head trabaja a partir de una "evaluacion perezosa"
 -- o lazy evaluation, entonces va a evaluar solo lo que necesita y despues tomar esos parametros.
+
 -- Ej.
+
 primerTecnicoEnCondiciones :: [Tecnico] -> Auto -> Tecnico
 primerTecnicoEnCondiciones unosTecnicos unAuto = head (tecnicosQueDejanElAutoEnCondiciones unosTecnicos unAuto)
 
